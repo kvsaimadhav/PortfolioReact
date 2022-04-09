895fce438de012b4ca42cfcbd9f9885a58c3716d
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2022-04-09 18:22:49 -0500

    Updates

---
+++ precache-manifest.cdcac1d88ef9c6a2596532053705a70c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "aca673148ba8cda67bc6",
+    "url": "/Portfolio-React/static/css/main.c85a0b15.chunk.css"
+  },
+  {
+    "revision": "aca673148ba8cda67bc6",
+    "url": "/Portfolio-React/static/js/main.e408c74b.chunk.js"
+  },
+  {
+    "revision": "9df23345e2872c440ab7",
+    "url": "/Portfolio-React/static/js/runtime~main.7eafcebd.js"
+  },
+  {
+    "revision": "caded9bbec325e711286",
+    "url": "/Portfolio-React/static/js/2.1753a2fc.chunk.js"
+  },
+  {
+    "revision": "03536cb0a75ce125b824",
+    "url": "/Portfolio-React/static/js/3.d0e5de46.chunk.js"
+  },
+  {
+    "revision": "09f90b9c4105fbce7fec1c96ce384af4",
+    "url": "/Portfolio-React/static/media/img1.09f90b9c.jpeg"
+  },
+  {
+    "revision": "092bedec9c9c525c1b22a5a36cc19301",
+    "url": "/Portfolio-React/static/media/icon1.092bedec.jpeg"
+  },
+  {
+    "revision": "cdb8bc74501355a432d352f61ab63a51",
+    "url": "/Portfolio-React/static/media/icon2.cdb8bc74.jpeg"
+  },
+  {
+    "revision": "7d80a7f767312553b75e55d5308d5831",
+    "url": "/Portfolio-React/static/media/icon3.7d80a7f7.jpeg"
+  },
+  {
+    "revision": "6c9974e59ef3cf8b3a05f2a7365ecfd5",
+    "url": "/Portfolio-React/static/media/icon4.6c9974e5.jpeg"
+  },
+  {
+    "revision": "007706d5283a0839252931d7aced9783",
+    "url": "/Portfolio-React/static/media/product1.007706d5.jpeg"
+  },
+  {
+    "revision": "3cab9255845e51ac171e3b20fc3632c4",
+    "url": "/Portfolio-React/static/media/product2.3cab9255.jpeg"
+  },
+  {
+    "revision": "f5b1df9781088f65800f9524f00c2234",
+    "url": "/Portfolio-React/static/media/product3.f5b1df97.jpeg"
+  },
+  {
+    "revision": "4656ba7775c991429b0e90a115f7f5a3",
+    "url": "/Portfolio-React/static/media/Resume.4656ba77.pdf"
+  },
+  {
+    "revision": "6e83b16b4da5cb85e5a4f8869761afb9",
+    "url": "/Portfolio-React/static/media/CV.6e83b16b.pdf"
+  },
+  {
+    "revision": "4d4a3aa2c1435ff466b53f75a9ad8a91",
+    "url": "/Portfolio-React/static/media/AWS.4d4a3aa2.pdf"
+  },
+  {
+    "revision": "1b3d1c1464653b3bc13612cb71374b48",
+    "url": "/Portfolio-React/static/media/banner.1b3d1c14.jpeg"
+  },
+  {
+    "revision": "b1b7640d5cba683a80565bdb44a1bae0",
+    "url": "/Portfolio-React/index.html"
+  }
+];
